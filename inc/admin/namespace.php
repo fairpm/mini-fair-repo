8510adc641fefc4d15b74289e96eb91205d6058e
Andy Fragen <andy@thefragens.com>
2025-11-22 09:34:51 -0800

    Display security contact with active packages (#73)
    Signed-off-by: Andy Fragen <andy@thefragens.com>

--- inc/admin/namespace.php
+++ inc/admin/namespace.php
@@ -111,6 +111,7 @@ function render_settings_page() {
 				<tr>
 					<th scope="col"><?php esc_html_e( 'Package ID', 'mini-fair' ); ?></th>
 					<th scope="col"><?php esc_html_e( 'Name', 'mini-fair' ); ?></th>
+					<th scope="col"><?php esc_html_e( 'Security Contact', 'mini-fair' ); ?></th>
 				</tr>
 			</thead>
 			<tbody>
@@ -122,10 +123,17 @@ function render_settings_page() {
 							continue;
 						}
 						$data = MiniFAIR\get_package_metadata( $did );
+						$security_contact = array_reduce(
+							$data->security,
+							fn ( $all, $current ) => array_merge( $all, array_values( $current ) ),
+							[]
+						);
+						$security_contact = implode( ', ', $security_contact ) ?: __( 'Not specified', 'mini-fair' );
 						?>
 						<td><code><?php echo esc_html( $package_id ); ?></code>
 							<a href="<?php echo esc_url( get_edit_post_link( $did->get_internal_post_id() ) ) ?>"><?php esc_html_e( '(View DID)', 'mini-fair' ) ?></a></td>
 						<td><?php echo esc_html( $data->name ); ?></td>
+						<td><?php echo esc_html( $security_contact ); ?></td>
 					</tr>
 				<?php endforeach; ?>
 		</table>
